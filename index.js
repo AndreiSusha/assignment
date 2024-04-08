ac35e925901a1e5a8606907b1bc9ff4aa95bc3f0
AndreiSusha <Andrey.Susha@gmail.com>
2024-04-08 13:54:18 +0300

    some files hase been added

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+// static files css, jpg etc
+app.use(express.static('public'));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
