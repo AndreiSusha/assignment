3c689d2ae3dfba2a2927fa325796191a3bf98934
AndreiSusha <Andrey.Susha@gmail.com>
2024-04-08 13:43:01 +0300

    added home page

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
+app.engine(
+  'handlebars',
+  exphbs.engine({
+    defaultLayout: 'main',
+  })
+);
+
+app.set('view engine', 'handlebars');
+
 app.get('/', (req, res) => {
-  res.send('Hello');
+  res.render('index');
 });
 
 const PORT = process.env.PORT || 3000;
