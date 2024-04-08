230aef32d8274694c711701e97758ad648355853
AndreiSusha <Andrey.Susha@gmail.com>
2024-04-08 14:37:43 +0300

    products

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
+const pizzas = [
+  {
+    name: 'Napoli',
+    price: 15,
+  },
+  {
+    name: 'Milano',
+    price: 16,
+  },
+  {
+    name: 'Margarita',
+    price: 15,
+  },
+];
+
 app.engine(
   'handlebars',
   exphbs.engine({
@@ -20,8 +35,19 @@ app.get('/contact', (req, res) => {
   res.render('contact');
 });
 
+app.get('/products', (req, res) => {
+  res.render('products', {
+    title: 'Menue',
+    products: pizzas,
+  });
+});
+
 // static files css, jpg etc
 app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  res.status(404).send('Sorry we dont have such pizzunja');
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
